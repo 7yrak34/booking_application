edc6e788f272f6285a8f63f7f4c978ea3e42e337
Zyrak Nur <noorzirak7@gmail.com>
2024-12-06 20:51:44 +0000

    add header and modify Icon component

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import './Header.scss';
+import { Icon } from '../Icon';
+import { IconType } from '../../types/IconType';
+import { useContext, useState } from 'react';
+import { ScreenSizeContext, ScreenType } from '../ScreenSizeProvider/ScreenSizeProvider';
+import { headerLinks } from '../../helpers/constants';
+
+export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const screenSize = useContext(ScreenSizeContext);
+
+  return (
+    <header className="header">
+      <Link to="/" className="header__link-logo">
+        <Icon iconType={IconType.logo} />
+      </Link>
+
+      {screenSize === ScreenType.isMobile && (
+        <button
+          type="button"
+          className="header__menu-button"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <Icon iconType={IconType.menu} isActive={isMenuOpen} />
+        </button>
+      )}
+
+      {screenSize !== ScreenType.isMobile && (
+        <nav className="header__nav">
+          <ul className="header__list">
+            {headerLinks.map(({ link, text }) => (
+              <li className="header__item">
+                <Link to={link} className="header__link">
+                  {text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
+}
