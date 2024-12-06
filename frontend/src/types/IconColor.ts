949a182cce20a03764b33f5393b558eb220ee134
Zyrak Nur <noorzirak7@gmail.com>
2024-12-06 12:07:12 +0000

    add Icon component

---
+++ frontend/src/types/IconColor.ts
@@ -0,0 +1,6 @@
+export enum IconColor {
+  whiteColor = "#fff",
+  primaryColor = "#FC6341",
+  secondaryColor = "#888888",
+  blackColor = "#000",
+}
---
+++ frontend/src/types/IconType.ts
@@ -0,0 +1,13 @@
+export enum IconType {
+  logo,
+  profile,
+  close,
+  menu,
+  location,
+  mail,
+  add,
+  calendar,
+  delete,
+  heart,
+  selectedHeart,
+}
