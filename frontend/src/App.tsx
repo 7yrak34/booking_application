ae4f56dffad3908955a0662e715dd45ef8b5fc8f
Zyrak Nur <noorzirak7@gmail.com>
2024-12-07 21:49:03 +0000

    hide menu on tablet and desktop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,21 @@ import './App.scss';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Menu } from './components/Menu';
+import { ScreenSizeContext, ScreenType } from './components/ScreenSizeProvider/ScreenSizeProvider';
 
 export const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const screenSize = useContext(ScreenSizeContext);
 
   return (
     <div className="app">
       <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
 
-      <Menu isMenuOpen={isMenuOpen} />
+      {screenSize === ScreenType.isMobile && (
+        <Menu isMenuOpen={isMenuOpen} />
+      )}
 
       <Outlet />
 
