b4c0703d17da344f00d14033b5a4587d9288df61
Zyrak Nur <noorzirak7@gmail.com>
2024-12-07 21:42:10 +0000

    create menu component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,17 @@ import './App.scss';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { useState } from 'react';
+import { Menu } from './components/Menu';
 
 export const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="app">
-      <Header />
+      <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+
+      <Menu isMenuOpen={isMenuOpen} />
 
       <Outlet />
 
